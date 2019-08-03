a8bfcc2252beaf2ebcb1ed723326f4aeb6102d3d
Rafael Ribeiro <tecinfo.rafael@gmail.com>
2019-08-03 00:10:40 -0300

    Handling exception on reading repositories file.

--- webscraping/data/files.py
+++ webscraping/data/files.py
@@ -1,4 +1,7 @@
 def open_file(filepath):
-    file = open(filepath, "r")
-    if file.mode == 'r':
-        return file.read()
+    try:
+        file = open(filepath, "r")
+        if file.mode == 'r':
+            return file.read()
+    except Exception as e:
+        raise Exception("Erro ao ler o arquivo %s\nMessage:\n%s" % (filepath, e))
